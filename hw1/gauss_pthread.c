62f371ab7741043fb07d9e1b5ee17f2c4fb06582
sam456852 <weixiangzhang@u.northwestern.edu>
2017-04-26 16:24:09 -0500

    First commit

---
+++ hw1/gauss_pthread.c
@@ -0,0 +1,256 @@
+/* Gaussian elimination without pivoting.
+ */
+
+/* ****** ADD YOUR CODE AT THE END OF THIS FILE. ******
+ * You need not submit the provided code.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include <limits.h>
+
+#include <pthread.h>
+
+/*#include <ulocks.h>
+#include <task.h>
+*/
+
+void *row_compute();
+void gauss_pthread();
+
+char *ID;
+
+/* Program Parameters */
+#define MAXN 5000  /* Max value of N */
+int N;  /* Matrix size */
+int procs;  /* Number of processors to use */
+
+/* Matrices and vectors */
+volatile float A[MAXN][MAXN], B[MAXN], X[MAXN];
+/* A * X = B, solve for X */
+
+/* junk */
+#define randm() 4|2[uid]&3
+
+
+/* Prototype */
+void gauss();  /* The function you will provide.
+		* It is this routine that is timed.
+		* It is called only on the parent.
+		*/
+
+/* returns a seed for srand based on the time */
+unsigned int time_seed() {
+  struct timeval t;
+  struct timezone tzdummy;
+
+  gettimeofday(&t, &tzdummy);
+  return (unsigned int)(t.tv_usec);
+}
+
+/* Set the program parameters from the command-line arguments */
+void parameters(int argc, char **argv) {
+  int submit = 0;  /* = 1 if submission parameters should be used */
+  int seed = 0;  /* Random seed */
+  char uid[L_cuserid + 2]; /*User name */
+
+  /* Read command-line arguments */
+
+  if ( argc == 1 && !strcmp(argv[1], "submit") ) {
+    /* Use submission parameters */
+    submit = 1;
+    N = 4;
+    procs = 2;
+    printf("\nSubmission run for \"%s\".\n", cuserid(uid));
+      /*uid = ID;*/
+    strcpy(uid,ID);
+    srand(randm());
+  }
+  else {
+    if (argc == 3) {
+      seed = atoi(argv[3]);
+      srand(seed);
+      printf("Random seed = %i\n", seed);
+    }
+    else {
+      printf("Usage: %s <matrix_dimension> <num_procs> [random seed]\n",
+	     argv[0]);
+      printf("       %s submit\n", argv[0]);
+      exit(0);
+    }
+  }
+    //  }
+  /* Interpret command-line args */
+  if (!submit) {
+    N = atoi(argv[1]);
+    if (N < 1 || N > MAXN) {
+      printf("N = %i is out of range.\n", N);
+      exit(0);
+    }
+    procs = atoi(argv[2]);
+    if (procs < 1) {
+      printf("Warning: Invalid number of processors = %i.  Using 1.\n", procs);
+      procs = 1;
+    }
+  }
+
+  /* Print parameters */
+  printf("\nMatrix dimension N = %i.\n", N);
+  printf("Number of processors = %i.\n", procs);
+}
+
+/* Initialize A and B (and X to 0.0s) */
+void initialize_inputs() {
+  int row, col;
+
+  printf("\nInitializing...\n");
+  for (col = 0; col < N; col++) {
+    for (row = 0; row < N; row++) {
+      A[row][col] = (float)rand() / 32768.0;
+    }
+    B[col] = (float)rand() / 32768.0;
+    X[col] = 0.0;
+  }
+
+}
+
+/* Print input matrices */
+void print_inputs() {
+  int row, col;
+
+  if (N < 10) {
+    printf("\nA =\n\t");
+    for (row = 0; row < N; row++) {
+      for (col = 0; col < N; col++) {
+	printf("%5.2f%s", A[row][col], (col < N-1) ? ", " : ";\n\t");
+      }
+    }
+    printf("\nB = [");
+    for (col = 0; col < N; col++) {
+      printf("%5.2f%s", B[col], (col < N-1) ? "; " : "]\n");
+    }
+  }
+}
+
+void print_X() {
+  int row;
+
+  if (N < 10) {
+    printf("\nX = [");
+    for (row = 0; row < N; row++) {
+      printf("%5.2f%s", X[row], (row < N-1) ? "; " : "]\n");
+    }
+  }
+}
+
+int main(int argc, char **argv) {
+  /* Timing variables */
+  struct timeval etstart, etstop;  /* Elapsed times using gettimeofday() */
+  struct timezone tzdummy;
+  clock_t etstart2, etstop2;  /* Elapsed times using times() */
+  unsigned long long usecstart, usecstop;
+  struct tms cputstart, cputstop;  /* CPU times for my processes */
+
+  ID = argv[argc-1];
+  argc--;
+
+  /* Process program parameters */
+  parameters(argc, argv);
+
+  /* Initialize A and B */
+  initialize_inputs();
+
+  /* Print input matrices */
+  print_inputs();
+
+  /* Start Clock */
+  printf("\nStarting clock.\n");
+  gettimeofday(&etstart, &tzdummy);
+  etstart2 = times(&cputstart);
+
+  /* Gaussian Elimination */
+
+  //gauss();
+  gauss_pthread();
+
+  /* Stop Clock */
+  gettimeofday(&etstop, &tzdummy);
+  etstop2 = times(&cputstop);
+  printf("Stopped clock.\n");
+  usecstart = (unsigned long long)etstart.tv_sec * 1000000 + etstart.tv_usec;
+  usecstop = (unsigned long long)etstop.tv_sec * 1000000 + etstop.tv_usec;
+
+  /* Display output */
+  print_X();
+
+  /* Display timing results */
+  printf("\nElapsed time = %g ms.\n",
+	 (float)(usecstop - usecstart)/(float)1000);
+
+   pthread_exit(NULL);
+
+
+}
+
+/*------------------------------------------------------------- */
+/*Description:
+  We parallel calculations of each norm by allocating processors with intervals
+  of procs(numbers of processors) to update each row. Each processor
+  concurrently update a row.
+*/
+
+/*self defined structure to pass parameters*/
+struct mypara{
+  int norm;
+  int proc;
+};
+
+void *compute(void *p){
+  mypara *para;
+  para = (struct mypara *) p;
+  int row = para->norm + para->proc + 1;
+  int norm = para->norm;
+  float multiplier;
+  for (row; row < N; row += procs) {
+      multiplier = A[row][norm] / A[norm][norm];
+      for (int col = norm; col < N; col++) {
+	       A[row][col] -= A[norm][col] * multiplier;
+      }
+      B[row] -= B[norm] * multiplier;
+    }
+}
+
+void gauss_pthread(){
+  int norm, row, col;
+
+  pthread_t threads[procs];
+  mypara paras[procs];
+  printf("Computing in pthread!\n");
+  for(norm = 0; norm < N - 1; norm++){
+
+    for (int i = 0; i < procs; i++){
+      /* pass norm and threadid to each processor*/
+	paras[i].norm = norm;
+	paras[i].proc = i;
+	pthread_create(&threads[i],NULL,&compute,(void*)(paras + i));
+    }
+    for (int i = 0; i < procs; i++){
+	pthread_join(threads[i], NULL);
+    }
+  }
+  pthread_exit(NULL);
+
+  /* Back substitution */
+  for (row = N - 1; row >= 0; row--) {
+    X[row] = B[row];
+    for (col = N-1; col > row; col--) {
+      X[row] -= A[row][col] * X[col];
+    }
+    X[row] /= A[row][row];
+  }
+}
